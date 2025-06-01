f86eaf2e2ac7ba897f6a227105c96e51ed30f2d1
Majid H Rebouh <magnetowasright325@gmail.com>
2025-06-01 14:40:01 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { supabase } from './supabaseClient';
+import { supabase } from './supabaseClient.js';
 import Header from './components/Header.jsx';
 import ActivityForm from './components/ActivityForm.jsx';
 import ActivityList from './components/ActivityList.jsx';
