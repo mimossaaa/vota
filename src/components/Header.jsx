cd77ed57c184675c31515e16512f318b805d042a
Majid H Rebouh <magnetowasright325@gmail.com>
2025-06-03 14:28:40 -0700

    trying to fix vercel errors

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
   return (
     <header className="w-full max-w-2xl text-center mb-8">
       <h1 className="text-4xl sm:text-5xl font-extrabold text-blue-700 leading-tight mb-4 animate-fade-in-down">
-        Vote on What the Bruce Does Next! 🚀
+        Vote on what Bruce does next! 🏀🐗
       </h1>
       <p className="text-xl text-gray-600">
         Suggest and upvote activity ideas for upcoming streams.
