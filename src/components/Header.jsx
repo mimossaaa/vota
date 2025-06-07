670d0cce1fd8b2a5261599c57815ef6921a8d3a4
Majid H Rebouh <magnetowasright325@gmail.com>
2025-06-06 20:25:00 -0700

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,10 @@ function Header() {
   return (
     <header className="w-full max-w-2xl text-center mb-8">
       <h1 className="text-4xl sm:text-5xl font-extrabold text-blue-700 leading-tight mb-4 animate-fade-in-down">
-        Vote on what Bruce does next! 🏀🐗
+        Vote on what Bruce does next! 
       </h1>
       <p className="text-xl text-gray-600">
-        ballhog shit
+      🏀🐗
       </p>
     </header>
   );
